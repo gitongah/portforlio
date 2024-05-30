51560d6f9c77f65c55addbb8d656cd4a6c967151
gitongah <mbaabugitonga@gmail.com>
2024-05-29 21:39:03 -0700

    made the contact form to send me an email when the user tries to contact me

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,31 @@ import call_icon from '../../assets/call_icon.svg'
 
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+  
+  const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+
+      formData.append("access_key", "c8558921-db5f-4ead-864f-addc5dbf8456");
+
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
   return (
     <div className='contact' id='contact'>
       <div className="contact-title">
@@ -29,7 +54,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form className='contact-right'>
+        <form onSubmit={onSubmit} className='contact-right'>
           <label htmlFor="">Your Name </label>
           <input type="text" placeholder='Enter your name' name='name' />
           <label htmlFor="">Your Email</label>
