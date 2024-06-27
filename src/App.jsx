27f4be6a232c600e7397d7d308cc29eee616ddd6
gitongah <mbaabugitonga@gmail.com>
2024-06-27 13:49:55 -0700

    added the experience section showing the eperience i have in the diferent technology

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
 import Services from './Components/Services/Services';
+import Experience from './Components/Experience/Experience';
 import MyWork from './Components/MyWork/MyWork';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Experience/>
       <Services/>
       <MyWork/>
       <Contact/>
