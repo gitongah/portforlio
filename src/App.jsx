721cabaa730aab6a4ecd7c98d8aa15bca1141540
gitongah <mbaabugitonga@gmail.com>
2024-06-28 13:18:28 -0700

    Added the navbar to help the user bee able to scroll to any section of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar } from './Components/Navbar/Navbar';
+import Navbar  from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
 import Services from './Components/Services/Services';
@@ -11,7 +11,7 @@ import Footer from './Components/Footer/Footer';
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Hero/>
       <About/>
       <Experience/>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,68 +1,21 @@
-import React, { useState } from 'react'
-import { useRef } from 'react';
+import React from 'react';
+import {useState} from 'react';
+import './Navbar.css';
+import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail, BiCodeCurly} from 'react-icons/bi';
+import {RiServiceLine} from 'react-icons/ri';
+import {Link} from 'react-scroll';
 
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
-import underline from '../../assets/nav_underline.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import menu_open from '../../assets/menu_open.svg'
-import menu_close from '../../assets/menu_close.svg'
-
-
-export const Navbar = () => {
-
-  window.addEventListener('scroll', function(){
-  const navbar=this.document.querySelector('.navbar');
-  navbar.classList.toggle('sticky', window.scrollY>0)
-})
-
-
-  const [menu, setMenu] = useState('')
-  const menuRef = useRef();
-
-  const openMenue = ()=>{
-    menuRef.current.style.right='0'
-  }
-
-  const closeMenue = ()=>{
-    menuRef.current.style.right='-350px'
-  }
+function Navbar() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div className='navbar'>
-
-      {/* <img src={logo} alt="Edwin Mbaabu logo" /> */}
-      <img className="nav-mob-open"src={menu_open} onClick= {openMenue }alt="" />
-      <ul className='nav-menu' ref={menuRef}>
-        <img className="nav-mob-close"src={menu_close} onClick={closeMenue}  alt="" />
-        <li>
-          <AnchorLink className='anchor-link' href='#home'>
-            <p onClick={() => setMenu('home')}>Home</p>
-          </AnchorLink>{menu === 'home' ? <img src={underline} alt=' ' /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className='anchor-link' offset={50} href='#about'>
-            <p onClick={() => setMenu('about')}>About Me</p>
-          </AnchorLink>{menu === 'about' ? <img src={underline} alt=' ' /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className='anchor-link' offset={50} href='#services'>
-            <p onClick={() => setMenu('services')}>Services</p>
-          </AnchorLink>{menu === 'services' ? <img src={underline} alt=' ' /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className='anchor-link' offset={50} href='#mywork'>
-            <p onClick={() => setMenu('mywork')}>Portfolio</p>
-          </AnchorLink>{menu === 'mywork' ? <img src={underline} alt=' ' /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className='anchor-link' offset={50} href='#contact'>
-            <p onClick={() => setMenu('contact')}>Contact</p>
-          </AnchorLink>{menu === 'contact' ? <img src={underline} alt=' ' /> : <></>}
-        </li>
-      </ul>
-
-      {/* <div className='nav-connect' > <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div> */}
-
-    </div>
+    <nav>
+      <Link to="header" spy={true} smooth={true} duration={500} activeClass='active' ><BiHomeAlt/></Link>
+      <Link to="about"  spy={true} smooth={true} duration={500} activeClass='active'><BiUser/></Link>
+      <Link to="experience"  spy={true}   smooth={true} duration={500} activeClass='active'><BiCodeCurly/></Link>
+      <Link to="portfolio"  spy={true}   smooth={true} duration={500} activeClass='active'><BiBook/></Link>
+      <Link to="contact"  spy={true}   smooth={true} duration={500} activeClass='active'><BiMessageSquareDetail/></Link>
+    </nav>
   )
 }
+
+export default Navbar
