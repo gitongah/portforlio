39b5cd741b3474158b7b5e45182ad2bfaef545d0
gitongah <mbaabugitonga@gmail.com>
2024-06-28 18:39:39 -0700

    removed files that i do not use in the file staructue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar  from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
-import Services from './Components/Services/Services';
 import Experience from './Components/Experience/Experience';
 import MyWork from './Components/MyWork/MyWork';
 import Contact from './Components/Contact/Contact';
@@ -15,7 +14,6 @@ const App = () => {
       <Hero/>
       <About/>
       <Experience/>
-      {/* <Services/> */}
       <MyWork/>
       <Contact/>
       <Footer/>
