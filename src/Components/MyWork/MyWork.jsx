782941b2c7e55a359593359d1df0b297d3c9663a
gitongah <mbaabugitonga@gmail.com>
2024-07-01 00:46:58 -0700

    added 2 new project in my work

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -4,8 +4,8 @@ import cover1 from "../../assets/bookSearch.png";
 import cover2 from "../../assets/noteTaker.png";
 import cover3 from "../../assets/bootPrint.png";
 import cover4 from "../../assets/employeeTracker.png";
-// import cover5 from "../../assets/portfolio5.png";
-// import cover6 from "../../assets/portfolio6.jpg";
+import cover5 from "../../assets/jsCalculator.png";
+import cover6 from "../../assets/typingSpeed.png";
 
 // IMAGES FOR PRIVATE USE ONLY (NO CREATIVE COMMONS)
 
@@ -37,6 +37,20 @@ const data = [
     title: "Employee Tracker",
     github: "https://github.com/gitongah/Employee-Tracker",
     demo: "https://drive.google.com/file/d/1U1xkw_nY7vJrgEuaTRGMa9xFS4EJLRCc/view",
+  },
+   {
+    id: 5,
+    image: cover5,
+    title: "A simple Calculator",
+    github: "https://github.com/gitongah/Calculator_JavaScript",
+    demo: "https://gitongah.github.io/Calculator_JavaScript/",
+  },
+  {
+    id: 6,
+    image: cover6,
+    title: "Typing Speed",
+    github: "https://github.com/gitongah/Typing-Speed",
+    demo: "https://gitongah.github.io/Typing-Speed/",
   },
 ];
 
